65f402874857acf0e46bf5675084699ab76d50b5
Shahvir <shahvirb@gmail.com>
2017-12-15 11:00:30 -0600

    1. moving boot.py logic largely into main.py to allow for directly running main.py to simplify dev workflow. 2. adding timezone hack

--- boot.py
+++ boot.py
@@ -5,34 +5,3 @@ import gc
 import webrepl
 webrepl.start()
 gc.collect()
-
-import leds
-pins = leds.init_pins()
-pins['D4'].on()
-
-def connect_wifi():
-    import network
-    sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
-        print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect('Win32TrojanKeyLogger.exe', '1234567890')
-        while not sta_if.isconnected():
-            pass
-    print('network config:', sta_if.ifconfig())
-
-connect_wifi()
-pins['D4'].off()
-
-# Create i2c bus and init display
-import machine
-i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4), freq=400000)
-from ssd1306 import SSD1306_I2C
-display = SSD1306_I2C(64, 48, i2c)
-
-# Set the RTC
-import ntptime
-ntptime.settime()
-
-import main
-main.main_loop(display)
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+from ssd1306 import SSD1306_I2C
 import ethprice
 import leds
+import machine
+import main
+import network
+import ntptime
 import time
 import utime
+
+
+def connect_wifi():
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('Win32TrojanKeyLogger.exe', '1234567890')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
 
 
 def get_price():
@@ -21,13 +37,33 @@ def display_price(display, price):
 def timestr(time):
     y, mon, d, h, m, s, wkday, yrday = time
     #HACK -- timezone adjustment
-    h += 18
+    h = int((h + 18) % 24)
     d -= 1
     return '{:02d}:{:02d}:{:02d}'.format(h, m, s)
 
 
-def main_loop(display):
+def main():
+    # Create i2c bus and init display
+    i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4), freq=400000)
+    display = SSD1306_I2C(64, 48, i2c)
+    
+    # Connect to WiFi
+    pins = leds.init_pins()
+    pins['D4'].on()
+    connect_wifi()
+    pins['D4'].off()
+    
+    # Set the RTC
+    ntptime.settime()
+    
     while True:
         price = get_price()
         display_price(display, price)
+        # for i in range(48):
+        #     display.scroll(0, 1); display.show()
+        #     time.sleep(0.1)
         time.sleep(30)
+
+
+if __name__ == '__main__':
+    main()
